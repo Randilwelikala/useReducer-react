db91e3ad53860155ff845861a91dd1879ddfd14d
randilwelikala <randilgimantha646@gmail.com>
2025-04-07 10:46:57 +0530

    create reducer function

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+const reducer = (state,action)=>{
+    console.log('Reducer Function');
+
+};
+
 
 const Counter = ()=>{
     const[count,setCount] = useState(0);
+
+    const[state,dispatch] = useReducer(reducer,{count:10});
     return(
         <div>
-             <h1>Count : {count}</h1>
+             {/* <h1>Count : {count}</h1> */}
+             <h1>Count : {state.count}</h1>
 
+{/* 
             <button onClick={()=>setCount(count+1)}>+</button>
             <br/>
             <button onClick ={()=>setCount(count-1)}>-</button>
             <br/>
-            <button onClick={()=>setCount(0)}>Reset</button>
+            <button onClick={()=>setCount(0)}>Reset</button> */}
+            <button onClick={()=>dispatch()}>+</button>
            
         </div>
     )
