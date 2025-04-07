4b649773c0d7fc48e233c56d4342c4a88abd693d
randilwelikala <randilgimantha646@gmail.com>
2025-04-07 11:25:43 +0530

    learnt useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter';
+import NameList from './NameList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Counter/>
+        <NameList/>
       </header>
     </div>
   );
--- src/NameList.jsx
+++ src/NameList.jsx
@@ -1,9 +1,40 @@
+import { type } from "@testing-library/user-event/dist/type";
+import { useReducer, useState } from "react";
+
+function reducer(nameList,action){
+    switch(action.type){
+        case 'add':
+            return {names:[...nameList.names,action.name]};
+        default:
+            return nameList;    
+    }
+
+};
+
+
 const NameList = ()=>{
-    <div>
-        return(
-            <P>hii</P>
-        )
-    </div>
+    const [name,setName]= useState('');
+
+    const [nameList,dispatch] = useReducer(reducer,{names:[]});
+
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+        dispatch({type: 'add',name:name});
+        setName('');
+    };
+    return(
+        <div>
+            <form action="" onSubmit={handleSubmit}>
+                <input onChange = {(event)=>setName(event.target.value)} type ="text" value={name}></input>
+            </form>
+
+            <p>
+                {nameList.names.join(', ')}
+            </p>
+        </div>
+    )
+    
 }
+    
 
 export default NameList;
